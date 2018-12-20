e4f4cfff540222805b021fe5301f339fec450b04
Jacopo Mangiavacchi <jamangia@microsoft.com>
2018-12-19 18:01:52 -0800

    Splitted from SwiftPipeline

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import LogisticRegressionLearnerTests
+
+var tests = [XCTestCaseEntry]()
+tests += LogisticRegressionLearnerTests.allTests()
+XCTMain(tests)
---
+++ Tests/LogisticRegressionLearnerTests/LogisticRegressionLearnerTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import LogisticRegressionLearner
+
+final class LogisticRegressionLearnerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LogisticRegressionLearner().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
